82c01b041063cec1de6445e1e06827e7b9a3b6b4
rakeshyadav36k <110667423+rakeshyadav36k@users.noreply.github.com>
2022-10-30 19:57:00 +0530

    printed the avg value of U,V and W i

---
+++ tut05/tut05.py
@@ -0,0 +1,61 @@
+
+
+# from datetime import datetime
+# start_time = datetime.now()
+
+# #Help https://youtu.be/N6PBd4XdnEw
+# def octant_range_names(mod=5000):
+
+    
+#     octant_id = {"1":"Internal outward interaction", "-1":"External outward interaction", "2":"External Ejection", "-2":"Internal Ejection", "3":"External inward interaction", "-3":"Internal inward interaction", "4":"Internal sweep", "-4":"External sweep"}
+
+# ###Code
+
+# from platform import python_version
+# ver = python_version()
+
+# if ver == "3.8.10":
+#     print("Correct Version Installed")
+# else:
+#     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+
+# mod=5000 
+# octant_range_names(mod)
+
+
+
+# #This shall be the last lines of the code.
+# end_time = datetime.now()
+# print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+
+from openpyxl import load_workbook
+import numpy as np
+import pandas as pd
+
+wb = load_workbook('octant_input.xlsx')     # I have opened the Excel file
+sheet = wb.active
+
+df = pd.read_excel('octant_input.xlsx')     # df is a data frame in which we put the data of excel file using pandas
+
+sheet.cell(row=1, column=5).value = "U Avg"       # Written the header 
+sheet.cell(row=1, column=6).value = "V Avg" 
+sheet.cell(row=1, column=7).value = "W Avg"  
+sheet.cell(row=1, column=8).value = "U'=U - U avg" 
+sheet.cell(row=1, column=9).value = "V'=V - V avg" 
+sheet.cell(row=1, column=10).value = "W'=W - W avg"
+
+Uavg = df['U'].mean()     # calculated the mean using pandas
+Vavg = df['V'].mean()
+Wavg = df['W'].mean()
+
+sheet.cell(row=2, column=5).value = Uavg   # added the average values in the sheet
+sheet.cell(row=2, column=6).value = Vavg 
+sheet.cell(row=2, column=7).value = Wavg 
+
+l1 = df['U']            # creating the list l1,l2,l3 which consist the element of U,V,W respectively
+l2 = df['V']
+l3 = df['W']
+
+
