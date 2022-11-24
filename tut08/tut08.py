449a59819a47dd3a979623e6113c786cf75c6a90
rakeshyadav36k <110667423+rakeshyadav36k@users.noreply.github.com>
2022-11-24 23:29:16 +0530

    Update tut08.py

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,30 +1,44 @@
 
+# from datetime import datetime
+# start_time = datetime.now()
+
+# #Help
+# def scorecard():
+# 	pass
+
+
+# ###Code
+
+# from platform import python_version
+# ver = python_version()
+
+# if ver == "3.8.10":
+# 	print("Correct Version Installed")
+# else:
+# 	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+
+# scorecard()
+
+
+
+
+
+
+# #This shall be the last lines of the code.
+# end_time = datetime.now()
+# print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+
+
+import sys
+from platform import python_version
+import re
+import pandas as pd
+
 from datetime import datetime
 start_time = datetime.now()
 
-#Help
-def scorecard():
-	pass
-
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-scorecard()
-
-
-
-
-
-
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+# here we open the scorecard file in write mode
+f = open("Scorecard.txt", 'w')
+sys.stdout = f
