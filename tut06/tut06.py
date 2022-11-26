99a8d74119f28e5645b8bdc6f3f4387ef830f711
rakeshyadav36k <110667423+rakeshyadav36k@users.noreply.github.com>
2022-11-26 23:49:48 +0530

    we create a list of registered  students & list of dates and created a dictionary to store the duplicate entries

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,3 +1,19 @@
+def attendance_report():
+    roll_numbers = [str(i) for i in registered_students['Roll No']]   # list of registered student
+
+    date_list = list({datetime.strptime(str(i).split(" ")[0],"%d-%m-%Y").date() for i in df['Timestamp']  if datetime.strptime(str(i).split(" ")[0],"%d-%m-%Y").strftime('%a') in ['Mon','Thu']})
+    #list of dates in which days are Monday and Thursday
+
+    date_list.sort()
+
+    duplicate = {date : {} for date in date_list}  # dictionary for storing the duplicate entries for each date
+
+    duplct_info = {roll_number : {date.strftime('%d-%m-%Y') : 0 for date in date_list} for roll_number in roll_numbers}
+    attended_dates = {roll_number : [] for roll_number in roll_numbers}
+    fake_atten=[]
+    fake_info = {roll_number : {date.strftime('%d-%m-%Y') : 0 for date in date_list} for roll_number in roll_numbers}
+    
+
 from platform import python_version    #  imported some libraries
 from datetime import datetime
 start_time = datetime.now()
@@ -11,7 +27,7 @@ df1=pd.DataFrame()
 df1['Roll']=registered_students['Roll No'].copy()
 df1['Name']=registered_students['Name'].copy()
 actl_atten=[]
-# attendance_report()
+attendance_report()
 
 df1.to_excel('./output/attendance_report_consolidated.xlsx',index=False)
 
