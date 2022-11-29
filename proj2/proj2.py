bed3b2816d32c1bcfe394fcc452d6d5720859415
rakeshyadav36k <110667423+rakeshyadav36k@users.noreply.github.com>
2022-11-29 18:48:49 +0530

    imported libraries and create a sidebar on webpage

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -1,30 +1,20 @@
-
+# imported some libraries
+import streamlit as st
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import Border, Side
+from openpyxl.styles import PatternFill
+import pandas as pd
+import os
 from datetime import datetime
+from openpyxl.utils.dataframe import dataframe_to_rows
+from streamlit_option_menu import option_menu
+
+
+
 start_time = datetime.now()
+st.set_page_config(page_title="Project 2",page_icon=":tada:",layout="wide")
 
-#Help
-def proj_octant_gui():
-	pass
-
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-proj_octant_gui()
-
-
-
-
-
-
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+with st.sidebar:
+    select = option_menu("Main Menu",["Home","Browse File","Add path"],icons=['house','files','folder'],menu_icon="laptop",default_index=0)
